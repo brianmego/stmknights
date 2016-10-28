fb961244c5c3d0b4b95fa175f380df62c6882da5
Brian Mego <brian.mego@q2ebanking.com>
2016-10-27 19:59:13 -0500

    Default to nuts view

--- campaigns/urls.py
+++ campaigns/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.degree_registration_new, name='degree_new'),
+    url(r'^$', views.nuts_order, name='nut_orders'),
     url(r'^degree/new/$', views.degree_registration_new, name='degree_new'),
     url(r'^degree/thankyou/(?P<pk>\d+)$', views.degree_thank_you, name='degree_thank_you'),
     url(r'^nuts/order/$', views.nuts_order, name='nut_orders'),
