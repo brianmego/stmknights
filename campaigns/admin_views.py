d1b38df55ab9fa9be91153c4e7b26b3700529d71
Brian Mego <brianmego@gmail.com>
2018-09-15 15:19:14 -0500

    Python 3.4.1 compliant date string

--- campaigns/admin_views.py
+++ campaigns/admin_views.py
@@ -110,7 +110,7 @@ def detail_report(request):
     header_row = ['Name', 'Date', 'Email', 'Order', 'Deferred', 'Extra']
     row_list = []
     for value in row_dict.values():
-        row_list.append([value['name'], value['date'].strftime('%D'), value['unique_id'], value['order'], value.get('deferred'), value.get('extra')])
+        row_list.append([value['name'], value['date'].strftime('%m/%d/%y'), value['unique_id'], value['order'], value.get('deferred'), value.get('extra')])
 
     row_list = sorted(row_list, key=lambda x: x[0].lower())
     substitutions = {
