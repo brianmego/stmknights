0a2d591c76bd2a059d15a6a77e44319d86eaa1d2
Brian Mego <brianmego@gmail.com>
2021-02-03 20:23:21 -0600

    Do not require /order

--- campaigns/urls.py
+++ campaigns/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^$', views.redirect_to_official, name='redirect'),
     url(r'^checkout/$', views.checkout_view, name='checkout'),
     url(r'^payment_confirmation/$', views.payment_confirmation_view, name='payment_confirmation'),
-    url(r'^(?P<campaign>[\w+]+)/order/(?P<pk>[\w+-]+)?$', views.generic_order, name='order'),
+    url(r'^(?P<campaign>[\w+]+)(?:/order)?/(?P<pk>[\w+-]+)?$', views.generic_order, name='order'),
 ]
